2d21270a4eef2ee42e20d98dfda6e55380530280
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-09-17 12:36:39 -0300

    comentei o footer inutilizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import style from './page.module.scss';
 import Header from '@/components/Header/Header';
 import CardSection from '@/components/CardSection/CardSection';
-import Footer from '@/components/Footer/Footer';
+// import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   const [isAbstractVisible, setIsAbstractVisible] = useState(false);
